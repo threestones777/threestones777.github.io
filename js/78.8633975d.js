ace09ed7954e0b2dba75f265b4d37ec3d1d2197d
threestones777 <wzl1427763314@163.com>
2024-10-17 16:55:06 +0800

    new start

--- js/78.8633975d.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkiMe"]=self["webpackChunkiMe"]||[]).push([[78],{78:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return i}});var r=n(65),s=n(593),o=n(587);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const i=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const a=(0,s.a)(i);a&&new Promise((e=>(0,o.c)(a,e))).then((()=>{(0,r.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,s.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.8633975d.js.map
